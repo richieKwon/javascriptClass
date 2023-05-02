2e4e10d8ecf4501e591d1e3ae3ceac8b7191530a
richie <richie.kwon1@gmail.com>
2023-05-02 22:02:49 +0100

    typescript

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-console.log(123)
+console.log('Cathy')
 
-const string1 = 'Hello world!'
 
-console.log(string1)
 
-const animals = new Array('apes', 'lion', 'dog')
 
-// const user = new Object()
 
-// user.name = 'Cathy'
-// user.age = 100
 
-function User() {
-    this.name = 'Cathy',
-        this.age = 100
-}
-const user = new User()
+
+// console.log(123)
+
+// const string1 = 'Hello world!'
+
+// console.log(string1)
+
+// const animals = new Array('apes', 'lion', 'dog')
+
+// // const user = new Object()
+
+// // user.name = 'Cathy'
+// // user.age = 100
+
+// function User() {  
+//     this.name = 'Cathy',
+//         this.age = 100
+// }
+// const user = new User()
 
